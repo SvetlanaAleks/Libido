21838257a7948f9e5794d729b0e4b05a15f1bade
Vladyslav Prodan <imvladprodan@gmail.com>
2019-05-09 14:14:23 +0300

    Vlad Prodan

--- app/components/industry/industry.js
+++ app/components/industry/industry.js
@@ -1,26 +1,37 @@
-$(".js-industry-slider").slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  prevArrow: $(".industry__arrow-prev"),
-  nextArrow: $(".industry__arrow-next"),
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
+$(".js-industry-slider")
+  .on("init", function(event, slick) {
+    $(this).append(
+      '<div class="slick-counter2"><span class="current2"></span> / <span class="total2"></span></div>'
+    );
+    $(".current2").text(slick.currentSlide + 1);
+    $(".total2").text(slick.slideCount);
+  })
+  .slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $(".industry__arrow-prev"),
+    nextArrow: $(".industry__arrow-next"),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+    ]
+  })
+  .on("beforeChange", function(event, slick, currentSlide, nextSlide) {
+    $(".current2").text(nextSlide + 1);
+  });
 
 $(document).ready(function() {
   $(".industry__button").click(function() {
--- app/components/reviews/reviews.js
+++ app/components/reviews/reviews.js
@@ -1,37 +1,56 @@
-$(".js-reviews-slider").slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  dots: true,
-  prevArrow: $(".reviews__arrow-prev"),
-  nextArrow: $(".reviews__arrow-next"),
-  centerMode: true,
-  centerPadding: "0px",
-  responsive: [
-    {
-      breakpoint: 1600,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3
+$(".js-reviews-slider")
+  .on("init", function(event, slick) {
+    $(this).append(
+      '<div class="slick-counter"><span class="current"></span> / <span class="total"></span></div>'
+    );
+    $(".current").text(slick.currentSlide + 1);
+    $(".total").text(slick.slideCount);
+  })
+  .slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    prevArrow: $(".reviews__arrow-prev"),
+    nextArrow: $(".reviews__arrow-next"),
+    centerMode: true,
+    centerPadding: "0px",
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          centerMode: false,
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
       }
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        centerMode: false,
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+    ]
+  })
+  .on("beforeChange", function(event, slick, currentSlide, nextSlide) {
+    $(".current").text(nextSlide + 1);
+  });
 
 $(document).ready(function() {
   $(".reviews__button").click(function() {
